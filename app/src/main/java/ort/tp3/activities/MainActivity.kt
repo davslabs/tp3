0eb75d1f6b3abffefac9891aac46ed2b046e62f2
David Ezequiel Assen <david.assen@securitize.io>
2023-05-17 23:50:30 +0200

    chore: crear template app

---
+++ app/src/main/java/ort/tp3/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package ort.tp3.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ort.tp3.R
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
