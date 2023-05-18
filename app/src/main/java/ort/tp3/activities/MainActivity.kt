52037951a96ffc4f7fc7091de67fd15be588f133
David Ezequiel Assen <david.assen@securitize.io>
2023-05-18 16:52:26 +0200

    feat: agregar item_character.xml layout para mostrar cada personaje, actualizar MainActivity.kt para mostrar el recyclerview con la lista de personajes

--- app/src/main/java/ort/tp3/activities/MainActivity.kt
+++ app/src/main/java/ort/tp3/activities/MainActivity.kt
@@ -1,12 +1,56 @@
 package ort.tp3.activities
 
+import ort.tp3.adapters.CharacterAdapter
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import ort.tp3.R
+import ort.tp3.services.CharacterService
+import ort.tp3.services.CharactersWrapper
+import ort.tp3.services.RetrofitInstance
+import retrofit2.Response
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CharacterAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        adapter = CharacterAdapter()
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+
+        CoroutineScope(Dispatchers.Main).launch {
+            fetchCharacters()
+        }
+    }
+
+    private suspend fun fetchCharacters() {
+        val service: CharacterService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(CharacterService::class.java)
+
+        // Hacemos la llamada a la API, seteamos por defecto 4000 personajes
+        val response: Response<CharactersWrapper> = service.getCharacters(4000)
+
+        if (response.isSuccessful) {
+            val characters = response.body()?.results
+
+            // Si la lista de personajes no es nula, la seteamos en el adapter
+            characters?.let {
+                adapter.setCharacterList(it)
+            }
+        } else {
+            println(response.errorBody())
+        }
     }
 }
